aefde0a150d4c6206400497b819f655f5322e08a
Pablo Cordova Morales <pccm_77@hotmail.com>
2017-05-13 21:05:30 -0500

    User service with jwt

---
+++ src/app/_services/user.service.ts
@@ -0,0 +1,54 @@
+/**
+ * The user service contains a standard set of CRUD methods for managin users, it contains a jwt() method that's used to
+ * add the JWT from local storage to the Authorization header of each http request
+ */
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOPtions, Response } from '@angular/http';
+
+import { User } from '../_models/user';
+
+
+@Injectable()
+export class UserService {
+
+    constructor(private http: Http) { }
+
+    // Get all users
+    getAll() {
+        return this.http.get('/api/users', this.jwt()).map((response: Response) => response.json());
+    }
+
+    // Get One user by ID
+    getById(id: number) {
+        return this.http.get('/api/users/' + id, this.jwt()).map((response: Response) => response.json());
+    }
+
+    // Create user
+    create(user: User) {
+        return this.http.post('/api/users', user, this.jwt()).map((response: Response) => response.json());
+    }
+
+    // Update user
+    update(user: User) {
+        return this.http.post('/api/users/' + user.id, user, this.jwt()).map((response: Response) => response.json());
+    }
+
+    // Delete user
+    delete(id: number) {
+        return this.http.delete('/api/users/' + id, this.jwt()).map((response: Response) => response.json());
+    }
+
+    // Private methods
+
+    private jwt() {
+        // Create autorization header with jwt token
+        let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        // TODO: Check if the token is needed to create users, I'm thinking not.
+        if (currentUser && currentUser.token) {
+            let headers = new Headers({ 'Authorization': 'Bearer' + currentUser.token });
+            return new RequestOptions({ headers: headers});
+        }
+    }
+
+
+}
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AlertService } from '../_services/alert.service';
+import { UserServices } from '../_services/user.service';
 
 @Component({
     selector: 'app-register',
@@ -10,13 +14,27 @@ export class RegisterComponent implements OnInit {
     model: any = {};
     loading = false;
 
-    constructor() { }
+    constructor() { 
+        private router : Router;
+        private userService: UserServices;
+        private alertService: AlertService;
+    }
 
     ngOnInit() {
     }
 
     register() {
         console.log("Enter register");
+        this.userService.create(this.model)
+            .subscribe(
+                data => {
+                    // Set success message and pass true parameter to persist the message after redirecting to the login page
+                    this.alertService.success('Registration successful', true);
+                    this.router.navigate(['/login']);
+                },
+                error => {
+                    this.alertService.error(error);
+                });
     }
 
 }
