6ba9845b4cf53cb82dc44670fa05e299b3dd639c
Pablo Cordova Morales <pccm_77@hotmail.com>
2017-06-20 23:38:53 -0500

    styles on navbar and lateral bar

--- src/app/lateral-bar/lateral-bar.component.ts
+++ src/app/lateral-bar/lateral-bar.component.ts
@@ -12,4 +12,21 @@ export class LateralBarComponent implements OnInit {
     ngOnInit() {
     }
 
+    /**
+     *    Event when a click is given in a title on home lateral bar
+     *
+     *    @param {string} idTitleLB - id current <a> tag
+     */
+
+    eventLateralBar(idTitleLB) {
+        // Get all title elements
+        let elementsCollapse = document.getElementsByClassName('titleLB');
+        // Format text color
+        [].forEach.call(elementsCollapse, function(elementCollapse) {
+            // The currunt title lateral bar paint with blue color and
+            // The other element with black color
+            elementCollapse.style.color = elementCollapse.id == idTitleLB ? '#0101DF' : 'black';
+        }); 
+    }
+
 }
