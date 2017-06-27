3a3eee61c763ef61aab31f2fae068dd71f098355
Pablo Cordova Morales <pccm_77@hotmail.com>
2017-06-26 23:59:15 -0500

    reorder item lateral bar

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,12 +3,22 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { SalesComponent } from './sales/sales.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { CustomersComponent} from './customers/customers.component';
+import { ReportsComponent } from './reports/reports.component';
+import { SettingsComponent } from './settings/settings.component';
 import { AuthGuard } from './_guards/auth.guard';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'sales', component: SalesComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'inventory', component: InventoryComponent },
+    { path: 'customers', component: CustomersComponent },
+    { path: 'reports', component: ReportsComponent },
+    { path: 'settings', component: SettingsComponent },
     { path: '**', redirectTo:'' }
 ];
 
